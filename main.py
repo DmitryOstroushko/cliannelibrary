66a728f88a67f8a042338162965126358af43e88
clianne <dmitrylianne@gmail.com>
2022-08-21 13:28:46 +0300

    working

--- main.py
+++ main.py
@@ -3,12 +3,14 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import string
 import sys
 from cliannelibrary.filelibrary import FileLibrary
 
 
 if __name__ == '__main__':
 
-    path = sys.argv[1] # "/home/clianne/ФОТО/"
-    FileLibrary().rename_file_by_mask(False, '*', 20210300)
+    path: string = sys.argv[1] # "/home/clianne/ФОТО/"
+    start_number: int = 20210300
+    FileLibrary().rename_file_by_mask(False, '*', start_number)
 
